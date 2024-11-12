4c5ffc0e8d5ece1d980622f1600f53fbd31788cf
PierreLaroseChomb <152908187+PierreLaroseChomb@users.noreply.github.com>
2024-11-12 15:29:35 +0100

    j

--- test.R
+++ test.R
@@ -1,9 +1,13 @@
-wrds <- dbConnect(
-  Postgres(),
-  host = "wrds-pgdata.wharton.upenn.edu",
-  dbname = "wrds",
-  port = 9737,
-  sslmode = "require",
-  user = Sys.getenv("wrds_user"),
-  password = Sys.getenv("wrds_password")
-)
+library(DBI)
+library(RPostgres)
+library(dplyr)
+library(dbplyr)
+
+mapping <- ccmxpf_linktable %>% left_join(compustat_all, by = c("gvkey"))
+
+
+merged_crsp_compustat_sub <- crsp_daily_light %>%
+  select(cusip, permno, date, prc, vol, shrout, bid, ask) %>%
+  left_join(mapping, by = c("permno"), relationship = "many-to-many") %>% 
+  filter(!is.na(gvkey) & 
+           (date >= linkdt & date <= linkenddt))
